9be9e355c9f4defe3e4791d578f232919261fee3
Carlos Galveias <carlos.galveias@pt.ibm.com>
2017-09-25 17:46:59 +0100

    fix for gkm issue

--- gkm.js
+++ gkm.js
@@ -1,22 +1,27 @@
 'use strict';
-
 // TODO: Verify if we're getting loaded from multiples location and prevent creating new child processes?
-
-var EventEmitter2 = require('eventemitter2').EventEmitter2;
+var EventEmitter2 = require('eventemitter2')
+  .EventEmitter2;
 var path = require('path');
-var spawn = require('child_process').spawn;
-
-var events = new EventEmitter2({wildcard: true});
+var spawn = require('child_process')
+  .spawn;
+var events = new EventEmitter2({ wildcard: true });
 var gkm = spawn('java', ['-jar', path.join(__dirname, 'lib/gkm.jar')]);
-
-gkm.stdout.on('data', function(data) {
-	data = data.toString().split(/\r\n|\r|\n/).filter(function(item) { return item; });
-	for (var i in data) {
-		var parts = data[i].split(':');
-		events.emit(parts[0], parts.slice(1));
-	}
+gkm.stdout.on('data', function (data) {
+  data = data.toString()
+    .split(/\r\n|\r|\n/)
+    .filter(function (item) { return item; });
+  for (var i in data) {
+    try {
+      var parts = data[i].split(':');
+      events.emit(parts[0], parts.slice(1));
+    } catch (e) {
+      // some events, like from ember js don't matter
+      // and they dont split causing a error.
+      // This solves it
+    }
+  }
 });
-
 module.exports = {
-	events: events
+  events: events
 };
