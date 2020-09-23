57eda5e35a6e06fe039e16734d8847efa5149ca0
Carlos Galveias <carlos.galveias@gmail.com>
2020-09-23 13:29:38 +0100

    using hasOwnProperty to avoid functions

--- gkm.js
+++ gkm.js
@@ -1,27 +1,28 @@
 'use strict';
+
 // TODO: Verify if we're getting loaded from multiples location and prevent creating new child processes?
-var EventEmitter2 = require('eventemitter2')
-  .EventEmitter2;
+
+var EventEmitter2 = require('eventemitter2').EventEmitter2;
 var path = require('path');
-var spawn = require('child_process')
-  .spawn;
+var spawn = require('child_process').spawn;
+
 var events = new EventEmitter2({ wildcard: true });
 var gkm = spawn('java', ['-jar', path.join(__dirname, 'lib/gkm.jar')]);
-gkm.stdout.on('data', function (data) {
-  data = data.toString()
-    .split(/\r\n|\r|\n/)
-    .filter(function (item) { return item; });
+
+gkm.stdout.on('data', function(data) {
+  data = data.toString().split(/\r\n|\r|\n/).filter(function(item) { return item; });
+  //console.log({ data });
   for (var i in data) {
+    if (!data.hasOwnProperty(i)) { continue; }
     try {
       var parts = data[i].split(':');
       events.emit(parts[0], parts.slice(1));
     } catch (e) {
-      // some events, like from ember js don't matter
-      // and they dont split causing a error.
-      // This solves it
+      //shhhh , dont emit stuff
     }
   }
 });
+
 module.exports = {
   events: events
 };
